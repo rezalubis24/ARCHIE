6287dc98821c3918a1ab7867771eb2af8dd05d35
Reza <rezafadhilah29@gmail.com>
2023-02-03 03:40:56 +0700

    Create Service Section

--- js/main.js
+++ js/main.js
@@ -3,3 +3,26 @@ window.addEventListener("scroll",function(){
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+
+// services section - modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add("active");
+}
+learnmoreBtns.forEach((learnmoreBtns, i) => {
+    learnmoreBtns.addEventListener("click",() => {
+        modal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtns) => {
+    modalCloseBtns.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
